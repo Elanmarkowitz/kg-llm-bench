a22d1b6c36a6abad4cd46e193b4881892b7f9ecd
Elan Markowitz <esmarkow@usc.edu>
2024-11-06 12:42:43 -0800

    fix graph samplers and improve

--- samplers/graph_samplers.py
+++ samplers/graph_samplers.py
@@ -5,7 +5,7 @@ import networkx as nx
 from kg_builder import KnowledgeGraph
 
 
-def sample_ego_graph_from_kg(self, kg: KnowledgeGraph, seed_entities, radius=1):
+def sample_ego_graph_from_kg(kg: KnowledgeGraph, seed_entities, radius=1):
     """
     Samples the ego graph of a given radius around the seed entities.
     
@@ -13,6 +13,7 @@ def sample_ego_graph_from_kg(self, kg: KnowledgeGraph, seed_entities, radius=1):
     :param radius: Radius of the ego graph.
     :return: A subgraph containing the ego graph.
     """
+    kg_copy = deepcopy(kg)
     ego_graphs = []
     for seed in seed_entities:
         ego_graph = nx.ego_graph(kg.graph, seed, radius=radius)
@@ -20,10 +21,16 @@ def sample_ego_graph_from_kg(self, kg: KnowledgeGraph, seed_entities, radius=1):
     
     # Combine all ego graphs into one subgraph
     combined_graph = nx.compose_all(ego_graphs)
-    return combined_graph
+    kg_copy.graph = combined_graph
+    refine_entities_and_relations_for_sample(kg_copy)
+    return kg_copy
     
 
-def prune_kg(kg: KnowledgeGraph, max_edges, max_degree):
+def prune_kg(kg: KnowledgeGraph, 
+             max_edges=None, 
+             max_degree=None,
+             max_out_degree=None,
+             max_in_degree=None):
     """
     Prunes the graph by removing edges and nodes based on the specified criteria.
     
@@ -34,23 +41,48 @@ def prune_kg(kg: KnowledgeGraph, max_edges, max_degree):
     kg_copy = deepcopy(kg)
     graph = kg_copy.graph
 
-    # Remove nodes based on the maximum degree
-    high_degree_nodes = [node for node, degree in graph.degree() if degree > max_degree]
-    # Sample edges to keep based on the maximum degree
-    edges_to_keep = set()
-    for node in high_degree_nodes:
-        edges = list(graph.edges(node))
-        if len(edges) > max_degree:
-            edges_to_keep.update(random.sample(edges, max_degree))
-        else:
-            edges_to_keep.update(edges)
-    
-    # Remove all edges not in the sampled set
-    edges_to_remove = set(graph.edges()) - edges_to_keep
-    graph.remove_edges_from(edges_to_remove)
+    # Process edges to ensure max out-degree is met
+    if max_out_degree is not None:
+        for node in graph.nodes:
+            out_degree = graph.out_degree(node)
+            if out_degree > max_out_degree:
+                edges = list(graph.out_edges(node))
+                edges_to_remove = random.sample(edges, out_degree - max_out_degree)
+                graph.remove_edges_from(edges_to_remove)
+
+    # Process nodes for in-degree
+    if max_in_degree is not None:
+        for node in graph.nodes:
+            in_degree = graph.in_degree(node)
+            if in_degree > max_in_degree:
+                edges = list(graph.in_edges(node))
+                edges_to_remove = random.sample(edges, in_degree - max_in_degree)
+                graph.remove_edges_from(edges_to_remove)
+
+    # Process nodes for total degree
+    if max_degree is not None:
+        for node in graph.nodes:
+            degree = graph.degree(node)
+            if degree > max_degree:
+                edges = list(graph.in_edges(node)) + list(graph.out_edges(node))
+                edges_to_remove = random.sample(edges, degree - max_degree)
+                graph.remove_edges_from(edges_to_remove)
     
     # Remove edges based on the maximum number of edges
-    edges_to_remove = random.sample(list(graph.edges()), len(graph.edges()) - max_edges)
-    graph.remove_edges_from(edges_to_remove)
+    if max_edges is not None and max_edges < len(graph.edges):
+        edges_to_remove = random.sample(list(graph.edges()), len(graph.edges()) - max_edges)
+        graph.remove_edges_from(edges_to_remove)
+    kg_copy.graph = graph  # not neccessary, but for clarity
     
+    refine_entities_and_relations_for_sample(kg_copy)
+
     return kg_copy
+
+
+def refine_entities_and_relations_for_sample(kg: KnowledgeGraph) -> KnowledgeGraph:
+    selected_nodes = kg.graph.nodes
+    selected_relations = set(rel for _, _, rel in kg.graph.edges(data='relation'))
+    kg.entities = {idx: ent for idx, ent in kg.entities.items() if idx in selected_nodes}
+    kg.core_nodes = {idx: ent for idx, ent in kg.core_nodes.items() if idx in selected_nodes}
+    kg.relations = {idx: rel for idx, rel in kg.relations.items() if rel.label in selected_relations}
+    return kg
