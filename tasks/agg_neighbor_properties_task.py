1439358f3509015c4d3c5e6fc6e18517e6cbaaae
Krupa Galiya <krupagaliya@gmail.com>
2024-12-18 22:54:25 +0530

    Add task for Agg neighbour properties

---
+++ tasks/agg_neighbor_properties_task.py
@@ -0,0 +1,125 @@
+from tasks.base_task import BaseTask
+from copy import deepcopy
+import os
+import random
+from kg_builder import KnowledgeGraph
+from samplers import graph_samplers
+import re
+
+
+class AggNeighborPropertiesTask(BaseTask):
+    """This task involves aggregating neighbor properties for an entity"""
+    def __init__(self, conversion_config, llm_config, pseudonomizer_config, dataset_file=None, results_file=None):
+        super().__init__("AggNeighborProperties",
+                         conversion_config,
+                         llm_config,
+                         pseudonomizer_config,
+                         dataset_file,
+                         results_file)
+
+    def run(self):
+        self.results = deepcopy(self.data)
+        for instance in self.results:
+            if instance is None:
+                continue
+            prompt = instance['prompt']
+            response = self.model(prompt)
+            instance['response'] = response
+            instance['score'] = self.evaluate_response(response, instance['answer'])
+        self.save_results()
+
+    def evaluate_response(self, response, answer):
+        match = re.search(r'Answer:\s*(\d+)', response)
+        extracted_number = int(match.group(1)) if match else None
+        if extracted_number is None:
+            return 0  # or some other logic to handle no number found
+        return 1 if extracted_number == int(answer[0]) else 0
+
+    def construct_instances(self, kg: KnowledgeGraph, num_instances=10, num_seed_entities=2, max_edges=100):
+        """Constructs instances for the task."""
+        for instance in range(num_instances):
+            seed_entities = random.sample(list(kg.core_nodes.keys()), num_seed_entities)
+            self.data.append(self.construct_instance(kg, seed_entities, instance, max_edges))
+
+    def construct_instance(self, kg: KnowledgeGraph, seed_entities, instance_id=0, max_edges=100):
+        sampled_kg = graph_samplers.sample_ego_graph_from_kg(kg, seed_entities, radius=2)
+
+        entities_with_multiple_edges = {ent for ent in sampled_kg.entities if len(sampled_kg.graph.edges(ent)) > 1}
+        sampled_kg.entities = {ent: sampled_kg.entities[ent] for ent in entities_with_multiple_edges}
+        sampled_kg.graph = sampled_kg.graph.subgraph(entities_with_multiple_edges).copy()
+
+        sampled_kg = graph_samplers.prune_kg(sampled_kg, max_edges=max_edges, max_degree=20)
+
+        text_kg = self.text_presenter.to_list_of_edges(sampled_kg)
+        count = 0
+        while count == 0:
+            anchor_ent = random.choice(list(sampled_kg.entities.keys())) # Get a random entity
+
+            relation = random.choice(list(sampled_kg.relations.values())).label # Get a random relation
+
+            neighbors_with_relation = [
+                neighbor for neighbor in sampled_kg.graph.neighbors(anchor_ent)
+                if sampled_kg.graph.get_edge_data(anchor_ent, neighbor).get('relation') == relation
+            ]
+
+            count = len(neighbors_with_relation)
+
+        question = f"Using the provided knowledge graph only answer the following question. How many neighbors of '{kg.entities[anchor_ent].label}' have a '{relation}' relation? Answer in the format 'Answer: <number>'."
+        prompt = self.structure_prompt(question, text_kg)
+
+        answer = [str(count)]
+
+        return {
+            'id': instance_id,
+            'prompt': prompt,
+            'question': question,
+            'anchor_ent': anchor_ent,
+            'relation': relation,
+            'text_kg': text_kg,
+            'answer': answer,
+            'seed_entities': seed_entities,
+            'kg': sampled_kg
+        }
+
+    def reformat_instances(self):
+        """Reformats self.data using self.text_presenter."""
+        for instance in self.data:
+            kg = instance['kg']
+            text_kg = self.text_presenter.to_list_of_edges(kg)
+            instance['text_kg'] = text_kg
+
+    def structure_prompt(self, question, text_kg):
+        intro = f"Your job is to answer questions using the following knowledge graph. {self.text_presenter.get_description()}. You must rely exclusively on the information presented in the Knowledge Graph to answer questions."
+        prompt = f"{intro}\n\nKnowledge Graph:\n{text_kg}\n\n{question}"
+        return prompt
+
+if __name__ == '__main__':
+    kg = KnowledgeGraph()
+
+    # Load entities and nodes
+    kg.load_entities('../data/countries/entities.tsv')
+    kg.load_core_nodes('../data/countries/nodes.tsv')
+
+    # Load relations
+    kg.load_relations('../data/countries/relations.tsv')
+
+    # Load edges and attributes
+    kg.load_edges('../data/countries/edges.tsv')
+    kg.load_attributes('../data/countries/attributes.tsv')
+
+    conversion_config = {'type': "list_of_edges"}
+    llm_config = {'model': 'gpt-4o-mini', 'provider': 'openai'}
+    pseudonomizer_config = {'pseudonym_file': '../data/countries/pseudonym_data/country_pseudonyms.tsv'}
+
+    task = AggNeighborPropertiesTask(conversion_config, llm_config, pseudonomizer_config)
+    seed_entities = random.sample(list(kg.core_nodes.keys()), 2)
+
+    task.load_dataset()
+
+    task.construct_instances(kg, num_instances=10, num_seed_entities=10, max_edges=100)
+
+    task.save_base_data()
+    task.save_dataset()
+
+    # task.run()
+    print("Finished..")
