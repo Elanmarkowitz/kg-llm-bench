ac4b41aaf458b0a74f60026952333ad1554ad8f1
Elan Markowitz <esmarkow@usc.edu>
2025-01-07 18:01:05 -0800

    update shortest path task to use new system

--- tasks/shortest_path_task.py
+++ tasks/shortest_path_task.py
@@ -1,8 +1,10 @@
+from typing import List
+from tqdm import tqdm
 from tasks.base_task import BaseTask
 from copy import deepcopy
 import os
 import random
-from kg_builder import KnowledgeGraph
+from kg_builder import Entity, KnowledgeGraph
 from samplers import graph_samplers
 import ast
 
@@ -15,17 +17,6 @@ class ShortestPathTask(BaseTask):
                          pseudonomizer_config,
                          dataset_file,
                          results_file)
-
-    def run(self):
-        self.results = deepcopy(self.data)
-        for instance in self.results:
-            if instance is None: # Skip instances that were not constructed
-                continue
-            prompt = instance['prompt']
-            response = self.model(prompt)
-            instance['response'] = response
-            instance['score'] = self.evaluate_response(response, instance['answer'])
-        self.save_results()
 
     def evaluate_response(self, response, answer):
         response = response.replace("SHORTEST PATH:", "").strip()
@@ -45,13 +36,16 @@ class ShortestPathTask(BaseTask):
 
     def construct_instances(self, kg: KnowledgeGraph, num_instances=10, num_seed_entities=2, max_edges=100):
         """Constructs instances for the task."""
+        print("Constructing base data")
         instances = 0
+        pbar = tqdm(total=num_instances)
         while instances < num_instances:
             seed_entities = random.sample(list(kg.core_nodes.keys()), num_seed_entities)
             instance = self.construct_instance(kg, seed_entities, instances, max_edges)
             if instance:
                 self.data.append(instance)
                 instances += 1
+                pbar.update()
 
     def construct_instance(self, kg: KnowledgeGraph, seed_entities, instance_id=0, max_edges=100):
         ent1, ent2 = seed_entities[:2]
@@ -88,33 +82,65 @@ class ShortestPathTask(BaseTask):
 
         add_path_to_graph(shortest_paths[0])
         shortest_paths = [shortest_paths[0]] + [path for path in shortest_paths[1:] if path_is_present(path)]
-            
-        text_kg = self.text_presenter.to_list_of_edges(sampled_kg)
 
-        answer_paths = [[kg.entities[node].label for node in path] for path in shortest_paths]
-
-        question = f"Your task is to find the shortest path from {answer_paths[0][0]} to {answer_paths[0][-1]}. For example, if the shortest path between Argentina and Mexico is through Bolivia and Colombia, then answer should be SHORTEST PATH: ['Argentina', 'Bolivia', 'Colombia', 'Mexico']. \n you should list your answer in the form list. \n\n What is the shortest path from {answer_paths[0][0]} to {answer_paths[0][-1]}? \n Answer: SHORTEST PATH:"
-
-        prompt = self.structure_prompt(question, text_kg)
-        answer = [f"SHORTEST PATH: {str(path)}" for path in answer_paths]
+        answer_paths: List[List[Entity]] = [[kg.entities[node] for node in path] for path in shortest_paths]
+        answer = self.answer(answer_paths)
+        question = self.question(answer_paths)
+        pseudo_kg = self.pseudonymize_kg(sampled_kg)
 
         return {
             'id': instance_id,
-            'prompt': prompt,
             'question': question,
-            'text_kg': text_kg,
             'shortest_paths': answer_paths,
             'answer': answer,
             'seed_entities': seed_entities,
-            'kg': sampled_kg
+            'kg': sampled_kg,
+            'pseudo_kg': pseudo_kg,
+            'pseudonomizer_mapping': self.pseudonomizer.copy_mapping()
         }
 
-    def reformat_instances(self):
-        """Reformats self.data using self.text_presenter."""
-        for instance in self.data:
-            kg = instance['kg']
-            text_kg = self.text_presenter.to_list_of_edges(kg)
+    def construct_formatted_instances(self):
+        """formats the kg and saves the formatted dataset (i.e. with actual prompt)"""
+        self.formatted_data = deepcopy(self.base_data)
+        for instance in self.formatted_data:
+            assert 'kg_path' in instance
+            kg = instance.pop('kg')
+
+            # task specific read instructions
+            instance['answer_paths'] = [[Entity.from_dict(e) for e in path] for path in instance['answer_paths']]
+
+            if self.pseudonomizer:
+                if 'pseudo_kg' in instance:
+                    kg = instance.pop('pseudo_kg')
+                else:
+                    if not 'pseudonomizer_mapping' in instance:
+                        raise ValueError("Pseudonomizer config set but no pseudonomizer mapping in the base data")
+                    self.pseudonomizer.load_mapping(instance['pseudonomizer_mapping'])
+                    kg = self.pseudonomizer.pseudonymize(kg)
+                    # task specific pseudonomize conversions
+                    instance['answer_paths'] = [[self.pseudonomizer.map_entity(e) for e in path] for path in instance['answer_paths']]
+            
+            # construct answer, question, text_kg, and prompt
+            instance['answer'] = self.answer(instance['answer_paths'])
+            question = self.question(instance['answer_paths'])
+            text_kg = self.text_presenter.convert(kg)
+
             instance['text_kg'] = text_kg
+            instance['prompt'] = self.structure_prompt(question, text_kg)
+            instance['question'] = question
+            # answer is a count so no change needed
+
+    @staticmethod
+    def question(answer_paths: List[List[Entity]]):
+        return f"Your task is to find the shortest path from {answer_paths[0][0].label} to {answer_paths[0][-1].label}. For example, if the shortest path between Argentina and Mexico is through Bolivia and Colombia, then answer should be SHORTEST PATH: ['Argentina', 'Bolivia', 'Colombia', 'Mexico']. \n you should list your answer in the form list. \n\n What is the shortest path from {answer_paths[0][0].label} to {answer_paths[0][-1].label}? \n Answer: SHORTEST PATH:"
+
+    @staticmethod
+    def answer(answer_paths: List[List[Entity]]):
+        return [f"SHORTEST PATH: {str(path)}" for path in answer_paths]
+
+    @staticmethod
+    def ent_path_to_label_path(path: List[Entity]):
+        return [e.label for e in path]
 
     def structure_prompt(self, question, text_kg):
         intro = f"Your job is to answer questions using the following knowledge graph. {self.text_presenter.get_description()}. You must rely exclusively on the information presented in the Knowledge Graph to answer questions."
@@ -142,14 +168,23 @@ if __name__ == '__main__':
 
 
     task = ShortestPathTask(conversion_config, llm_config, pseudonomizer_config)
-    seed_entities = random.sample(list(kg.core_nodes.keys()), 2)
 
-    task.load_dataset()
+    try:
+        task.load_base_dataset()
+    except ValueError:
+        task.construct_base_instances(kg, num_instances=10, num_seed_entities=10, max_edges=100)
+        task.save_base_dataset()
+    
+    try:
+        task.load_formatted_dataset()
+    except ValueError:
+        task.construct_formatted_instances()
+        task.save_formatted_dataset()
 
-    task.construct_instances(kg, num_instances=10, num_seed_entities=10, max_edges=100)
-
-    task.save_base_data()
-    task.save_dataset()
+    try:
+        task.run()
+    except ValueError:
+        print("No LLM configured, skipping run")
 
     # task.run()
     print("Finished..")
